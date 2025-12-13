e03db627a166f1e266ca1a6026a5f052e84980e6
amc2040 <ambercarr_04@yahoo.com>
2025-12-12 19:57:45 -0600

    Update app.py

--- app.py
+++ app.py
@@ -252,4 +252,4 @@ def delete_reservation(reservation_id):
     return redirect(url_for('admin_login'))
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
